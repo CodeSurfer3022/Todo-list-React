c627f30f2ee1c3e63913c3c925efb00d59d3b183
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-08-15 20:39:07 +0530

    Updates

---
+++ precache-manifest.8b54a39891aa06c90c5c74f4fda7edfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9482315cb843701c4b47ea831fcfe7",
+    "url": "/Todo-list-React/index.html"
+  },
+  {
+    "revision": "3340e4023cab6645ee5f",
+    "url": "/Todo-list-React/static/css/main.02ec1ba1.chunk.css"
+  },
+  {
+    "revision": "8d9aa49f3f38b9d64288",
+    "url": "/Todo-list-React/static/js/2.478c8af7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-list-React/static/js/2.478c8af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3340e4023cab6645ee5f",
+    "url": "/Todo-list-React/static/js/main.2f066a3f.chunk.js"
+  },
+  {
+    "revision": "22103d383863aa6af315",
+    "url": "/Todo-list-React/static/js/runtime-main.ae636639.js"
+  }
+]);

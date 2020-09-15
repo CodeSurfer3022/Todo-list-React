6e1a6d0178b62b67b1c59b66dcb3e77fa3589872
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-09-15 21:44:16 +0530

    Change style on checking item

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: 'italic',
+        color: '#cdcdcd',
+        textDecoration: 'line-through'
+    }
     return(
         <div className="todo">
             <input type="checkbox" checked={props.completed} onChange={props.handleChange}/>
-            <p>{props.text}</p>
+            <p style={props.completed ? completedStyle: null}>{props.text}</p>
         </div>
     );
 }
